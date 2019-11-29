b557b2a0fd83f14d06eca129503c848e141c14fa
Renan Zanelato <renanzanelato96@gmail.com>
2019-11-28 23:14:52 -0300

    Entity Player Unit Test

---
+++ RockPaperScissor.Test/Entities/PlayerTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RockPaperScissors.Entities;
+using System;
+
+namespace RockPaperScissors.Test.Entities
+{
+    [TestFixture]
+    public class PlayerTest
+    {
+        [Test]
+        public void ShouldMoveBeUppercase()
+        {
+          
+            Player Armando = new Player("Armando", 's');
+            Armando.Move.Should().Be('S');
+        }
+    }
+}
